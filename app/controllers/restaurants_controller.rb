5d617c2cd171c0773f2f70ec9d554a0ae348a8c4
wilson cely <wectcorp@gmail.com>
2022-04-19 21:38:09 +0200

    exercise ended

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,36 @@ def index
   @restaurants = Restaurant.all
 end
 
-private
-def find_restaurant
-  @restaurant = Restaurant.find(params[:id])
+def show
 end
+
+def new
+ @restaurant = Restaurant.new
+end
+
+def create
+  @restaurant = Restaurant.new(restaurant_params)
+  if @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  else
+    render :new
+  end
+end
+
+
+  private
+
+  def find_restaurant
+   @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+
+
+
 
 
 end
